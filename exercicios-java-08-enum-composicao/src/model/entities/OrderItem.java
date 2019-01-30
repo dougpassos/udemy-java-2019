4435322be84a9642462b4b70c6991ae88dbe586d
Douglas Passos <douglas.santos.qa@gmail.com>
2019-01-30 18:39:26 -0200

    fim do exercicio 8 e add gitignore demais projetos

---
+++ exercicios-java-08-enum-composicao/src/model/entities/OrderItem.java
@@ -0,0 +1,42 @@
+package model.entities;
+
+public class OrderItem {
+
+	private Integer quantity;
+	private Double price;
+	private Product product;
+
+	public OrderItem(Integer quantity, Double price, Product product) {
+
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	public Integer getquantity() {
+		return quantity;
+	}
+
+	public void setquantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public double subTotal() {
+		return price * quantity;
+	}
+
+	@Override
+	public String toString() {
+		return product.getName() + ", $" + String.format("%.2f", price) + ", Quantity: " + quantity + ", Subtotal: $"
+				+ String.format("%.2f", subTotal());
+	}
+
+}
